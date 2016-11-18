287d2ce24225b96761faf94391cc7db0c38ed4e3
ryan <ryanpback@gmail.com>
2016-11-17 23:58:24 -0800

    refactor word_count.rb down to 12 lines of code.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,13 +1,12 @@
 class String
   define_method(:word_count) do |word_to_count|
-    number_of_words = 0
-    sentence_array = self.downcase().split(" ")
+    count_words = {'number_of_words' => 0, 'sentence_array' => self.downcase().split(" ")}
 
-    sentence_array.each() do |word|
+    count_words['sentence_array'].each() do |word|
       if word.match(/#{word_to_count}/)
-        number_of_words+= 1
+        count_words['number_of_words']+= 1
       end
     end
-    number_of_words
+    count_words.fetch('number_of_words')
   end
 end
