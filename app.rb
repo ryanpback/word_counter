031894d42b6d1ac7288be55774aae4a506b48c6f
ryan <ryanpback@gmail.com>
2016-11-18 16:32:55 -0800

    change functionality to allow the user to search for the word independent of capitalization.

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ end
 
 get('/word_finder') do
   @user_input = params.fetch('user_string')
-  @user_word = params.fetch('word_select')
+  @user_word = params.fetch('word_select').downcase()
   @user_search = params.fetch('select')
   @result = params.fetch('user_string').word_count(@user_word, @user_search)
   erb(:word_count_output)
